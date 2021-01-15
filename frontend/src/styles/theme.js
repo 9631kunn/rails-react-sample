02fae53a151edeb94a21a73999ffa53312ec46ea
9631kunn <infourbh+1@gmail.com>
2021-01-15 13:30:09 +0900

    add: theme

---
+++ frontend/src/styles/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+  color: {
+    main: "#26a5bb",
+    border: "#eee",
+    text: "#666",
+    button: "#eee",
+  },
+  font: {
+    h2: "30px",
+    h3: "24px",
+    h4: "16px",
+    h5: "14px",
+  },
+};
